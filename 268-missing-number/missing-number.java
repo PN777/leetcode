eb4e8665ad4378723a8cc73c0a5b0cc0bc4a6f26
Nischal <pnischal425@gmail.com>
2026-09-13 19:21:16 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (82.04%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
+        int result = nums.length;
 
-        int expected = n * (n + 1)/2;
-            
-        int actual = 0;
-        for(int num: nums){
-        actual += num;
+        for(int i = 0; i < nums.length; i++){
+            result ^= i;
+            result ^= nums[i];
         }
-        return expected - actual;
+        return result;
     }
 }
